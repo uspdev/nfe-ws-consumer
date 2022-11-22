dbd25ba8005e7ceb8430a6aeda7b20ca9880df81
Masaki K Neto <kawabata@usp.br>
2022-11-22 07:58:50 -0300

    reorganizando código

--- README.md
+++ README.md
@@ -32,3 +32,102 @@ $ret = $sefaz->consultaXML($xml);
 
 print_r($ret);
 ```
+
+Resposta
+```
+Array
+(
+    [status] => ok
+    [url] => Array
+        (
+            [xml] => http://servidor/nfe-ws/xml/chave-da-nfe-nfe.xml
+            [proto] => http://servidor/nfe-ws/prot/chave-da-nfe-prot.xml
+            [sefaz] => http://servidor/nfe-ws/sefaz/chave-da-nfe-prot.pdf
+            [danfe] => http://servidor/nfe-ws/danfe/chave-da-nfe-danfe.pdf
+        )
+
+    [xml] => Array
+        (
+            [status] => ok
+            [estrutura] => Estrutura do XML está OK
+            [assinatura] => Assinatura ok
+            [digest] => Digest ok
+            [modelo] => 55
+            [import] => Importado com sucesso
+            [versao] => 4.00
+        )
+
+    [chave] => chave-da-nfe
+    [prot] => Array
+        (
+            [age] => 20
+            [cStat] => 100
+            [xMotivo] => Autorizado o uso da NF-e
+            [tpAmb] => 1
+            [dhConsulta] => 21/11/2022 - 15:05:46
+            [status] => ok
+            [eventos] => Array
+                (
+                    [0] => Array
+                        (
+                            [tpEvento] => 100
+                            [descEvento] => Autorizado o uso da NF-e
+                            [nProt] => 135221386812053
+                            [dhEvento] => 10/10/2022 - 16:14:16
+                            [digVal] => vmQ/8gcUAxVUTKl7UzulmDess+Q=
+                        )
+
+                )
+
+            [raw] => <?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body>
+            ...
+            </soap:Body></soap:Envelope>
+        )
+
+    [sefaz] => Array
+        (
+            [age] => 20
+            [cStat] => 100
+            [xMotivo] => Autorizado o uso da NF-e
+            [dhConsulta] => 21/11/2022 - 15:05:46
+            [tpAmb] => 1
+            [versao] => uspdev/NFE-WS v2.0.7
+        )
+
+    [nfe] => Array
+        (
+            [ide] => Array
+                (
+                    [nro] => 999
+                    [serie] => 111
+                    [dataemi] => 10/10/2022 - 16:13:25
+                    [total] => 180,00
+                )
+
+            [emit] => Array
+                (
+                    [cnpj] => CNPJ EMIT
+                    [nome] => RAZÃO SOCIAL
+                    [mun] => MUNICÍPIO
+                    [uf] => SP
+                )
+
+            [dest] => Array
+                (
+                    [cnpj] => CNPJ/CPF DEST
+                    [nome] => NOME DEST
+                )
+
+            [infadic] => DADOS ADICIONAIS DA NFE
+        )
+)
+```
+
+## Métodos
+
+* status()
+* consultaXML()
+* obterDanfe()
+* obterSefaz()
+* obterArquivo()
+
--- src/NfeWsConsumer.php
+++ src/NfeWsConsumer.php
@@ -8,8 +8,11 @@ class NfeWsConsumer
     protected $srv;
     protected $usr;
     protected $pwd;
+    protected $debug = 0; // 0 - desativado, 1 - somente erros, 2 - mensagens echo
 
-    public function __construct($srv, $usr, $pwd)
+    protected $connectTimeout = 15;
+
+    public function __construct($srv, $usr, $pwd, $debug = 0)
     {
         if ('/' != substr($srv, -1)) {
             $srv = $srv . '/';
@@ -17,6 +20,11 @@ class NfeWsConsumer
         $this->srv = $srv;
         $this->usr = $usr;
         $this->pwd = $pwd;
+        $this->debug = $debug;
+    }
+
+    public function setDebug($debug) {
+        $this->debug = $debug;
     }
 
     /**
@@ -26,30 +34,11 @@ class NfeWsConsumer
      */
     public function status()
     {
-        $endpoint = 'status';
-
-        $curl = curl_init($this->srv . $endpoint);
-
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_USERPWD, $this->usr . ":" . $this->pwd);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded; charset=UTF-8"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        #curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        //curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-        #curl_setopt($curl, CURLOPT_VERBOSE, true); // somente para debug
-
-        $json_response = curl_exec($curl);
-
-        // verifica error
-        if ($res = $this->curlErrors($curl)) {
-            return $res;
+        if ($this->debug == 2) {
+            echo 'debug: consultaXML: ', $this->srv . 'status', PHP_EOL;
         }
-
-        curl_close($curl);
-
-        $response = json_decode($json_response, true);
-        return $response;
+        $status = $this->obterArquivo($this->srv . 'status');
+        return $status;
     }
 
     /**
@@ -61,65 +50,125 @@ class NfeWsConsumer
      */
     public function consultaChave($chave)
     {
-
     }
 
     /**
-     * consultaXML
-     *
      * Envia um XML de NFE para o servidor e retorna a resposta da consulta.
+     *
      * O servidor valida o XML e retorna a resposta de consulta do XML junto à sefaz
+     * incluindo links para protocolo, danfe, protocolo, etc
      * Em geral as respostas são válidas até 6 meses da emissão
      * Depois disso a sefaz pode remover a consulta retornando status ??
-     * documento antigo ??? (ou algo assim)
+     *
+     * nao iremos verificar a integridade do $xml
      *
      * @param  String $xml String contendo o xml da NFE
-     *
      * @return Array Retorna o resultado da consulta e dados da NFE no formato array
      */
     public function consultaXML($xml)
     {
-        // nao iremos verificar a integridade do $xml
+        $endpoint = $this->srv . 'xml';
 
-        $endpoint = 'xml';
+        if ($this->debug == 2) {
+            echo 'debug: consultaXML: ', $endpoint, PHP_EOL;
+        }
 
-        $curl = curl_init($this->srv . $endpoint);
+        $content = 'xml=' . rawurlencode($xml); // equivalente a curl_escape()
+        $response = $this->chamar($endpoint, $content);
+        return json_decode($response, true);
+    }
 
-        // precisamos escapar pois alguns xmls tem caracteres especiais
-        $content = 'xml=' . curl_escape($curl, $xml);
+    /**
+     * Retorna o DANFE do xml fornecido
+     */
+    public function obterDanfe($xml)
+    {
+        $consulta = $this->consultaXML($xml);
+
+        if ($consulta['status'] == 'ok') {
+            return SELF::obterArquivo($consulta['url']['danfe']);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Retorna o relatório de consulta À SEFAZ do xml fornecido
+     */
+    public function obterSefaz($xml)
+    {
+        $consulta = $this->consultaXML($xml);
+
+        if ($consulta['status'] == 'ok') {
+            return SELF::obterArquivo($consulta['url']['sefaz']);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Executa a chamada de obter arquivo no servidor remoto
+     *
+     * @param $url
+     * @return Filestream
+     */
+    public function obterArquivo($url)
+    {
+        $context = stream_context_create([
+            'http' => ['header' => "Authorization: Basic " . base64_encode("$this->usr:$this->pwd")],
+        ]);
+        return file_get_contents($url, false, $context);
+    }
+
+    /**
+     * Realiza a chamada CURL no servidor
+     *
+     * @param String $endpoint
+     * @param String $content
+     * @return Resposta do CURL
+     */
+    protected function chamar($endpoint, $content = null)
+    {
+        $curl = curl_init($endpoint);
+
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $this->connectTimeout);
+        curl_setopt($curl, CURLOPT_TIMEOUT, $this->connectTimeout); //timeout in seconds
 
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_USERPWD, $this->usr . ":" . $this->pwd);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded; charset=UTF-8"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-        #curl_setopt($curl, CURLOPT_VERBOSE, true); // somente para debug
+        if ($content) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        } else {
+            curl_setopt($curl, CURLOPT_POST, false);
+        }
+        if ($this->debug > 0) {
+            curl_setopt($curl, CURLOPT_VERBOSE, true); // somente para debug
+        }
 
-        $json_response = curl_exec($curl);
+        $response = curl_exec($curl);
 
         if ($res = $this->curlErrors($curl)) {
             return $res;
         }
-
         curl_close($curl);
 
-        $response = json_decode($json_response, true);
         return $response;
     }
 
-    private function curlErrors($ch) {
-        // em caso de erros
+    // em caso de erros
+    private function curlErrors($ch)
+    {
         if (curl_errno($ch)) {
-            return '('.curl_errno($ch) . ') ' . curl_error($ch) . PHP_EOL;
+            return '(' . curl_errno($ch) . ') ' . curl_error($ch) . PHP_EOL;
         }
-
         $response = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
         if ('200' != $response) {
             return 'Erro: CURLINFO_RESPONSE_CODE=' . $response . PHP_EOL;
         }
-
         return false;
     }
 }
